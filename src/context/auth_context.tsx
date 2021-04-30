b724c94664f276db7d7fd2c65fb10686c89a0508
huanzhiqiang <huanzhiqiang@jiangduoduo.com>
2021-04-30 16:02:15 +0800

    feat: add  loading

--- src/context/auth_context.tsx
+++ src/context/auth_context.tsx
@@ -1,8 +1,10 @@
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { ReactNode, useEffect } from "react";
 import { IAuthParams } from "../api/auth_provider";
 import * as auth from "../api/auth_provider";
 import { IUser } from "../pages/projectList/components/SearchPanel";
 import { http } from "../utils/http";
+import { useAsync } from "../utils/use-async";
+import { FullPageErrorFallback, FullPageLoading } from "../components/lib";
 
 const AuthContext = React.createContext<
   | {
@@ -27,13 +29,28 @@ const bootstrapUser = async () => {
 };
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<IUser | null>(null);
+  const {
+    run,
+    data: user,
+    error,
+    isLoading,
+    isError,
+    isIdle,
+    setData: setUser,
+  } = useAsync<IUser | null>();
   const login = (form: IAuthParams) => auth.login(form).then(setUser);
   const register = (form: IAuthParams) => auth.register(form).then(setUser);
   const logout = () => auth.logout().then(() => setUser(null));
   useEffect(() => {
-    bootstrapUser().then(setUser);
+    run(bootstrapUser());
   }, []);
+
+  if (isIdle || isLoading) {
+    return <FullPageLoading />;
+  }
+  if (isError) {
+    return <FullPageErrorFallback error={error} />;
+  }
   return (
     <AuthContext.Provider
       value={{ user, login, register, logout }}
